49ccda166d22d4002c1f3f6dd0453e686930fab6
danielluccio <daniel130877@gmail.com>
2025-01-08 18:13:26 -0300

    Pagina de Login Configurada

--- website/views.py
+++ website/views.py
@@ -1,4 +1,36 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        username = request.POST.get('username', '').strip()
+        password = request.POST.get('password', '').strip()
+
+        if not username or not password:
+            messages.error(request, "Username and password are required!")
+            return render(request, 'home.html')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            template_name = 'home.html'
+            login(request, user)
+            messages.success(request, 'You have been logged in successfully!')
+            return render(request, template_name)
+        else:
+            messages.error(request, "There was an error logging in. Please try again.")
+            return render(request, 'home.html')  # Re-renderiza a página com mensagens de erro
+    else:
+        return render(request, 'home.html')
+
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
+
+
